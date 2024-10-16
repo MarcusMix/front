7898f81bc1440806d5badefb6ffc7a70a9690092
MarcusMix <tradedomarcus@gmail.com>
2024-10-16 19:59:37 -0300

    fix: router pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/home/Home';
-// import About from './pages/About';
+import Login from './pages/login/login';
 
 function App() {
   return (
@@ -12,14 +12,14 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            {/* <Link to="/about">About</Link> */}
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
