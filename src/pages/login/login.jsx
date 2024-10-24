9ac32d93214801e3f2d6f9327bb2dab0fbd27777
MarcusMix <tradedomarcus@gmail.com>
2024-10-24 19:46:41 -0300

    feat: login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,27 @@
+// Login.js
 import React from 'react';
+import './login.css'; // Importar o CSS associado
+import Input from '../../components/input/input';
+import Button from '../../components/button/button';
 
-function Login() {
+
+const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="form-box">
+      <form className="form">
+        <span className="title">Faça login</span>
+        <span className="subtitle">Crie uma conta grátis.</span>
+        <div className="form-container">
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Senha" />
+        </div>
+        <Button type="submit" label="Realizar login" />
+      </form>
+      <div className="form-section">
+        <p>Não tem uma conta? <a href="/cadastrar">Cadastrar-se</a></p>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
