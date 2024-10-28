c14b95afa3a6cbda3dd376be7345b9114ecb0451
MarcusMix <tradedomarcus@gmail.com>
2024-10-28 20:55:16 -0300

    feat: add page service provider sign

---
+++ src/pages/signup-service/signup-service.jsx
@@ -0,0 +1,41 @@
+// pages/SignUpService.js
+import React from 'react';
+import './signup-service.css'; // Importar o CSS específico
+import FormBox from '../../components/form-box/FormBox';
+import FormContainer from '../../components/form-container/FormContainer';
+import Button from '../../components/button/button';
+import Input from '../../components/input/input';
+import Subtitle from '../../components/subtitle/Subtitle';
+import TitleNew from '../../components/title/Title';
+
+
+const SignUpService = () => {
+  return (
+    <div className="fix">
+    <FormBox>
+      <form className="form">
+        <TitleNew>Conta de Prestador de Serviço.</TitleNew>
+        <Subtitle>Dados pessoais</Subtitle> 
+        <FormContainer>
+          <Input type="nome" placeholder="Nome completo" />
+          {/* mudar descricao para area */}
+          <Input type="descricao" placeholder="Descrição do perfil" />
+          <Input type="experiencia" placeholder="Experiência" />
+          <Input type="imagem" placeholder="Imagem" />
+        </FormContainer>
+        <Subtitle>Endereço</Subtitle> 
+        <FormContainer>
+          <Input type="estado" placeholder="Estado" />
+          <Input type="cidade" placeholder="Cidade" />
+          <Input type="bairro" placeholder="Bairro" />
+          <Input type="rua" placeholder="Rua" />
+          <Input type="numero" placeholder="Número" />
+        </FormContainer>
+        <Button type="submit" label="Cadastrar Perfil de Prestador" />
+      </form>
+    </FormBox>
+    </div>
+  );
+};
+
+export default SignUpService;
