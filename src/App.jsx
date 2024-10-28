5a3361137e424f6bc096a525debebf25ee6aef1c
MarcusMix <tradedomarcus@gmail.com>
2024-10-28 20:53:02 -0300

    feat: add routes sign user and service provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/login';
 import ServiceDetails from './pages/services/services';
+import SignUpUser from './pages/signup-user/signup-user';
+import SignUpService from './pages/signup-service/signup-service';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup-user" element={<SignUpUser />} />
+        <Route path="/signup-service" element={<SignUpService/>} />
         <Route path="/service/:id" element={<ServiceDetails/>} />
       </Routes>
     </Router>
