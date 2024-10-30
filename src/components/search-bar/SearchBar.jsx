3601d146e99d4fa8bfe643f7a65b4d84a88e0cc7
MarcusMix <tradedomarcus@gmail.com>
2024-10-30 20:58:35 -0300

    feat: add menu item user

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -8,6 +8,9 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -16,8 +19,7 @@ const Search = styled('div')(({ theme }) => ({
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  marginLeft: 0,
-  width: '100%',
+  width: '200px', // Define uma largura fixa
   [theme.breakpoints.up('sm')]: {
     marginLeft: theme.spacing(1),
     width: 'auto',
@@ -42,32 +44,37 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     [theme.breakpoints.up('sm')]: {
-      width: '30ch',
+      width: '200px', // Define uma largura fixa
       '&:focus': {
-        width: '40ch',
+        width: '250px', // Aumenta a largura ao focar
       },
-    '&::placeholder': {
-      color: '#f8f8f8', // Altere esta cor para qualquer que você queira
-      opacity: 1, // Para garantir que o placeholder seja totalmente visível
-    },
     },
   },
 }));
 
 export default function SearchAppBar() {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar 
         position="fixed" 
         sx={{ 
           width: '100%', 
-          height: '60px', // Define a altura da barra para 100% da tela
-          display: 'flex',
-          justifyContent: 'center', 
           backgroundColor: '#FFC107',
         }}
       >
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
           <IconButton
             size="large"
             edge="start"
@@ -85,7 +92,8 @@ export default function SearchAppBar() {
           >
             Marketplace do Job
           </Typography>
-          <Search>
+          
+          <Search sx={{ mx: 2 }}>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
@@ -94,6 +102,25 @@ export default function SearchAppBar() {
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search>
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              anchorEl={anchorElUser}
+              keepMounted
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
