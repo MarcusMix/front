e32614bf12f064242a9cbd2ba00d79dc984d8261
MarcusMix <tradedomarcus@gmail.com>
2024-10-30 21:12:44 -0300

    feat: add config user page and route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/login/login';
 import ServiceDetails from './pages/services/services';
 import SignUpUser from './pages/signup-user/signup-user';
 import SignUpService from './pages/signup-service/signup-service';
+import ConfigUser from './pages/config-user/config-user';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup-user" element={<SignUpUser />} />
         <Route path="/signup-service" element={<SignUpService/>} />
         <Route path="/service/:id" element={<ServiceDetails/>} />
+        <Route path="/config-user" element={<ConfigUser/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/config-user/config-user.jsx
@@ -0,0 +1,30 @@
+// Login.js
+import React from 'react';
+import Button from '../../components/button/button';
+import { Box, Container } from '@mui/material';
+import TitleNew from '../../components/title/Title';
+import { Link } from 'react-router-dom';
+
+
+const ConfigUser = () => {
+  return (
+    <Container maxWidth="sm" >
+          <Box 
+        sx={{ 
+          display: 'flex', 
+          flexDirection: 'column', // Alinha os itens em coluna
+          gap: 2, // Espaço entre os itens
+          padding: 2 // Espaço interno do container
+        }}
+      >
+
+
+        <TitleNew>Bem-vindo ao seu perfil!</TitleNew>
+        <Link to="/signup-service"> <Button label="Criar Perfil de Prestador" /> </Link> 
+      </Box>
+    </Container>
+
+  );
+};
+
+export default ConfigUser;
