b1039ecf66cd7745677da2bda6c060cabed5a288
MarcusMix <tradedomarcus@gmail.com>
2024-11-10 15:19:25 -0300

    fix: implements login auth and save token in local storage

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,19 +1,48 @@
 // Login.js
-import React from 'react';
-import './login.css'; // Importar o CSS associado
+import React, { useState } from 'react';
+import './login.css';
 import Input from '../../components/input/input';
 import Button from '../../components/button/button';
+import getDataFunction from '../../api/api';
+
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  
+  const handleLoginUser = async (event) => {
+    event.preventDefault();
+  
+    try {
+      const response = await getDataFunction("auth/login", "POST", JSON.stringify({ email, password }));
+      
+      if (response) { 
+        console.log("ok");
+        localStorage.setItem("token", response); 
+        toast.success('Login realizado com sucesso!');
+        navigate('/');
+      } else {
+        toast.error('Falha no login. Verifique suas credenciais.');
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error('Erro de conexão com o servidor.');
+    }
+  }
+
   return (
     <div className="form-box">
-      <form className="form">
+      <form className="form" onSubmit={handleLoginUser}>
         <span className="title">Faça login</span>
         <span className="subtitle">Crie uma conta grátis.</span>
         <div className="form-container">
-          <Input type="email" placeholder="Email" />
-          <Input type="password" placeholder="Senha" />
+          <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+          <Input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
         </div>
         <Button type="submit" label="Realizar login" />
       </form>
