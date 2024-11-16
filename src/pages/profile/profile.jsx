35b2d846b42e669e629cb13a0bd388ebd7f22adc
MarcusMix <tradedomarcus@gmail.com>
2024-11-16 10:24:39 -0300

    feat: (error) user profile page

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,29 +1,77 @@
-// Login.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../components/button/button';
 import { Box, Container } from '@mui/material';
 import TitleNew from '../../components/title/Title';
 import { Link } from 'react-router-dom';
-
+import {jwtDecode} from 'jwt-decode'; // Remova as chaves para o import funcionar corretamente
 
 const Profile = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const token = localStorage.getItem('token');
+
+  let loggedInUsername = null;
+
+  // Decodifica o token
+  if (token) {
+    try {
+      const decoded = jwtDecode(token);
+      loggedInUsername = decoded.sub;
+    } catch (error) {
+      console.error("Token inválido:", error);
+    }
+  }
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/user?email=${loggedInUsername}`, // Email como parâmetro de consulta
+          {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${token}`, // Token no cabeçalho
+            },
+          }
+        );
+
+        // Verifica se a resposta é válida
+        if (!response.ok) {
+          throw new Error(`Erro na requisição: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setUserInfo(data);
+      } catch (error) {
+        console.error("Erro ao buscar informações do usuário:", error);
+      }
+    };
+
+    if (loggedInUsername && token) {
+      fetchUserInfo();
+    }
+  }, [loggedInUsername, token]);
+
+  if (!userInfo) {
+    return <div>Carregando...</div>;
+  }
+
   return (
-    <Container maxWidth="sm" >
-          <Box 
-        sx={{ 
-          display: 'flex', 
-          flexDirection: 'column', // Alinha os itens em coluna
-          gap: 2, // Espaço entre os itens
-          padding: 2 // Espaço interno do container
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          padding: 2,
         }}
       >
-
-
         <TitleNew>Bem-vindo ao seu perfil!</TitleNew>
-        <Link to="/signup-service"> <Button label="Criar Perfil de Prestador" /> </Link> 
+        <p>{userInfo.name}</p>
+        <Link to="/signup-service">
+          <Button label="Criar Perfil de Prestador" />
+        </Link>
       </Box>
     </Container>
-
   );
 };
 
