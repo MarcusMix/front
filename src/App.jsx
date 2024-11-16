0354e84d1f7e87163e548f30e256fe4ae86d3ed8
Wesley Dutra <Wesley-Dutra17@hotmail.com>
2024-11-16 00:26:51 -0300

    feat: redirection to the provider's page

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import ServiceDetails from './pages/services/services';
 import SignUpUser from './pages/signup-user/signup-user';
 import SignUpService from './pages/signup-service/signup-service';
 import Profile from './pages/profile/profile';
+import ProviderProfile from './pages/providerProfile/providerProfile'; // Importe o componente
 
 function App() {
   return (
     <Router>
-        <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup-user" element={<SignUpUser />} />
-        <Route path="/signup-service" element={<SignUpService/>} />
-        <Route path="/service/:id" element={<ServiceDetails/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signup-service" element={<SignUpService />} />
+        <Route path="/service/:id" element={<ServiceDetails />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/provider-profile/:id" element={<ProviderProfile />} />
       </Routes>
     </Router>
   );
