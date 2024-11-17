6e85a2cc0b5d8084a9fb64687dd2edfb8cf3e009
Wesley Dutra <Wesley-Dutra17@hotmail.com>
2024-11-17 04:50:31 -0300

    feat: Added logic to hire a service

--- src/components/offered-service-card/OfferedServiceCard.jsx
+++ src/components/offered-service-card/OfferedServiceCard.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { Card, CardContent, CardMedia, Typography, Button } from '@mui/material';
+import axios from 'axios'; // Importe o axios
+import { jwtDecode } from 'jwt-decode'; // Importe a biblioteca
 
 function OfferedServiceCard({ service }) {
     const { name, description, price, image } = service;
+
+    const handleHireService = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const decodedToken = jwtDecode(token);
+            const userId = decodedToken.userId; // Obtém o ID do usuário do token
+
+            const serviceOrderDTO = {
+                userId: userId,
+                serviceProviderId: service.serviceProvider.id,
+                offeredServiceId: service.id,
+                status: 'PENDING',
+            };
+
+            await axios.post('http://localhost:8080/service-order', serviceOrderDTO, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            alert('Serviço contratado com sucesso!');
+        } catch (error) {
+            console.error('Erro ao contratar serviço:', error);
+            alert('Erro ao contratar serviço. Por favor, tente novamente.');
+        }
+    };
 
     return (
         <Card>
@@ -25,7 +53,7 @@ function OfferedServiceCard({ service }) {
                     Preço: R$ {price}
                 </Typography>
             </CardContent>
-            <Button variant="contained" fullWidth>
+            <Button variant="contained" fullWidth onClick={handleHireService}> {/* Adicione o onClick */}
                 Contratar Serviço
             </Button>
         </Card>
