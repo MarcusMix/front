4ecc1222ad7ae94b4bd04f794b94f2db9ff40096
MarcusMix <tradedomarcus@gmail.com>
2024-11-17 12:53:47 -0300

    feat: hire a new service

--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -4,6 +4,9 @@ import { Container, Typography, Card, Grid, Avatar, Box, CircularProgress } from
 import { styled } from '@mui/system';
 import getDataFunction from '../../api/api';
 import Button from '../../components/button/button';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import { toast } from 'react-hot-toast';
 
 const StyledCard = styled(Card)(({ theme }) => ({
   maxWidth: '600px',
@@ -25,43 +28,55 @@ const ServiceProviderName = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(2),
 }));
 
-const ServiceInfo = styled(Typography)(({ theme }) => ({
-  marginTop: theme.spacing(2),
-  textAlign: 'center',
-}));
-
 function ServiceDetails() {
-  const { id } = useParams();
+  const { id } = useParams(); // Obter o ID da URL
   const [serviceProvider, setServiceProvider] = useState(null);
   const [offeredService, setOfferedService] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // Fetch dos detalhes do prestador e serviço oferecido
   useEffect(() => {
-    async function fetchServiceProvider() {
+    const fetchDetails = async () => {
       try {
-        const data = await getDataFunction(`service-provider/${id}`);
-        setServiceProvider(data);
-        console.log("service", data)
-      } catch (error) {
-        console.error('Erro ao buscar os detalhes do prestador de serviço:', error);
-      }
-    }
+        const serviceData = await getDataFunction(`offered-service/${id}`);
+        setOfferedService(serviceData);
 
-    async function fetchOfferedService() {
-      try {
-        const data = await getDataFunction(`offered-service/${id}`);
-        setOfferedService(data);
-        console.log("ofered", data)
+        const providerData = await getDataFunction(`service-provider/${serviceData.serviceProviderId}`);
+        setServiceProvider(providerData);
       } catch (error) {
-        console.error('Erro ao buscar os detalhes do serviço oferecido:', error);
+        console.error('Erro ao buscar os detalhes do serviço ou prestador:', error);
       } finally {
         setLoading(false);
       }
-    }
+    };
 
-    fetchServiceProvider();
-    fetchOfferedService();
+    fetchDetails();
   }, [id]);
+
+  const handleHireService = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.userId; // Obtém o ID do usuário do token
+
+      const serviceOrderDTO = {
+        userId: userId,
+        serviceProviderId: serviceProvider.id,
+        offeredServiceId: offeredService.id,
+        status: 'PENDING',
+      };
+
+      await axios.post('http://localhost:8080/service-order', serviceOrderDTO, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      toast.success('Serviço contratado com sucesso!')
+    } catch (error) {
+      console.error('Erro ao contratar serviço:', error);
+      toast.error('Erro ao contratar serviço. Por favor, tente novamente.')
+    }
+  };
 
   if (loading) {
     return (
@@ -83,12 +98,11 @@ function ServiceDetails() {
     <Container>
       <StyledCard>
         <AvatarContainer>
-        <Avatar
-          sx={{ width: 80, height: 80 }}
-          src={serviceProvider.image ? `data:image/jpeg;base64,${serviceProvider.image}` : '/default-avatar.png'}
-          alt={serviceProvider.name}
-        />
-
+          <Avatar 
+            sx={{ width: 80, height: 80 }}
+            src={serviceProvider.image ? `data:image/jpeg;base64,${serviceProvider.image}` : '/default-avatar.png'}
+            alt={serviceProvider.name}
+          />
         </AvatarContainer>
         <ServiceProviderName variant="h4">{serviceProvider.name}</ServiceProviderName>
         <Typography variant="body1" align="center" color="textSecondary" paragraph>
@@ -97,9 +111,8 @@ function ServiceDetails() {
         <Typography variant="body1" align="center" color="textSecondary" paragraph>
           Descrição: {serviceProvider.description || 'Nenhuma descrição disponível.'}
         </Typography>
-        
+
         {/* Informações do serviço oferecido */}
-        {/* <ServiceInfo variant="h5">Serviço Oferecido</ServiceInfo> */}
         <Typography
           variant="h4"
           align="center"
@@ -112,25 +125,25 @@ function ServiceDetails() {
         <Typography variant="body1" align="center" color="textSecondary" paragraph>
           {offeredService.description}
         </Typography>
-        
+
         <img
           src={offeredService.image ? `data:image/jpeg;base64,${offeredService.image}` : '/default-avatar.png'}
           alt=""
           style={{
-            display: 'block', // Centraliza horizontalmente
-            margin: '0 auto', // Centraliza horizontalmente
-            width: '250px', // Define a largura fixa
-            height: '250px', // Define a altura fixa
-            objectFit: 'cover', // Mantém a proporção da imagem, cortando o excesso
-            borderRadius: '8px', // Opcional: bordas arredondadas
+            display: 'block',
+            margin: '0 auto',
+            width: '250px',
+            height: '250px',
+            objectFit: 'cover',
+            borderRadius: '8px',
           }}
         />
-          <Typography variant="h6" align="center" color="grey">
-            Preço: R$ {offeredService.price.toFixed(2)}
-          </Typography>
+        <Typography variant="h6" align="center" color="grey">
+          Preço: R$ {offeredService.price.toFixed(2)}
+        </Typography>
 
         <Box display="flex" justifyContent="center" marginTop={2}>
-        <Button type="submit" label="Contratar serviço" />
+          <Button type="submit" label="Contratar Serviço" onClick={handleHireService} />
         </Box>
       </StyledCard>
     </Container>
