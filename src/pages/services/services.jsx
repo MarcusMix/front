99f556f22c65a8dd30c7655931a73496e66450f7
MarcusMix <tradedomarcus@gmail.com>
2024-11-17 12:00:03 -0300

    feat: add offered service image and service provider too

--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -41,6 +41,7 @@ function ServiceDetails() {
       try {
         const data = await getDataFunction(`service-provider/${id}`);
         setServiceProvider(data);
+        console.log("service", data)
       } catch (error) {
         console.error('Erro ao buscar os detalhes do prestador de serviço:', error);
       }
@@ -50,6 +51,7 @@ function ServiceDetails() {
       try {
         const data = await getDataFunction(`offered-service/${id}`);
         setOfferedService(data);
+        console.log("ofered", data)
       } catch (error) {
         console.error('Erro ao buscar os detalhes do serviço oferecido:', error);
       } finally {
@@ -81,11 +83,12 @@ function ServiceDetails() {
     <Container>
       <StyledCard>
         <AvatarContainer>
-          <Avatar
-            sx={{ width: 80, height: 80 }}
-            src={serviceProvider.image || '/default-avatar.png'}
-            alt={serviceProvider.name}
-          />
+        <Avatar
+          sx={{ width: 80, height: 80 }}
+          src={serviceProvider.image ? `data:image/jpeg;base64,${serviceProvider.image}` : '/default-avatar.png'}
+          alt={serviceProvider.name}
+        />
+
         </AvatarContainer>
         <ServiceProviderName variant="h4">{serviceProvider.name}</ServiceProviderName>
         <Typography variant="body1" align="center" color="textSecondary" paragraph>
@@ -96,18 +99,36 @@ function ServiceDetails() {
         </Typography>
         
         {/* Informações do serviço oferecido */}
-        <ServiceInfo variant="h5">Serviço Oferecido</ServiceInfo>
-        <Typography variant="body1" align="center" paragraph>
+        {/* <ServiceInfo variant="h5">Serviço Oferecido</ServiceInfo> */}
+        <Typography
+          variant="h4"
+          align="center"
+          color="black"
+          sx={{ fontWeight: 'bold', marginBottom: 2 }}
+        >
           {offeredService.name}
         </Typography>
+
         <Typography variant="body1" align="center" color="textSecondary" paragraph>
           {offeredService.description}
         </Typography>
-        <Typography variant="h6" align="center" color="primary">
-          Preço: R$ {offeredService.price.toFixed(2)}
-        </Typography>
         
-        {/* Centraliza o botão */}
+        <img
+          src={offeredService.image ? `data:image/jpeg;base64,${offeredService.image}` : '/default-avatar.png'}
+          alt=""
+          style={{
+            display: 'block', // Centraliza horizontalmente
+            margin: '0 auto', // Centraliza horizontalmente
+            width: '250px', // Define a largura fixa
+            height: '250px', // Define a altura fixa
+            objectFit: 'cover', // Mantém a proporção da imagem, cortando o excesso
+            borderRadius: '8px', // Opcional: bordas arredondadas
+          }}
+        />
+          <Typography variant="h6" align="center" color="grey">
+            Preço: R$ {offeredService.price.toFixed(2)}
+          </Typography>
+
         <Box display="flex" justifyContent="center" marginTop={2}>
         <Button type="submit" label="Contratar serviço" />
         </Box>
