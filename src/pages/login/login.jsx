18870305edb03f4e99de16f0dff542a988cf0725
MarcusMix <tradedomarcus@gmail.com>
2024-11-20 17:00:48 -0300

    style: tiny modifications in css

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import './login.css';
 import Input from '../../components/input/input';
-import Button from '../../components/button/button';
+import MyButton from '../../components/button/button';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import loginUser from '../../api/login';
@@ -14,18 +14,18 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const { login } = useContext(AuthContext);
-  
+
   const handleLoginUser = async (event) => {
     event.preventDefault();
-  
+
     try {
       const response = await loginUser("auth/login", "POST", JSON.stringify({ email, password }));
-      
-      if (response) { 
+
+      if (response) {
         //login auth context
         login(response)
-        
-        localStorage.setItem("token", response); 
+
+        localStorage.setItem("token", response);
         toast.success('Login realizado com sucesso!');
         navigate('/');
       } else {
@@ -40,13 +40,13 @@ const Login = () => {
   return (
     <div className="form-box">
       <form className="form" onSubmit={handleLoginUser}>
-        <span className="title">Faça login</span>
+        <span className="title" color="#FFF">Faça login</span>
         <span className="subtitle">Crie uma conta grátis.</span>
         <div className="form-container">
-          <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+          <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
           <Input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <Button type="submit" label="Realizar login" />
+        <MyButton type="submit" label="Realizar login" />
       </form>
       <div className="form-section">
         <p>Não tem uma conta? <a href="/cadastrar">Cadastrar-se</a></p>
